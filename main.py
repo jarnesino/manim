49782e1a7c21c97c56ad3984b41069cebf439fe6
jarnesino <julianezequielarnesino@gmail.com>
2025-02-11 11:18:28 -0300

    Add another working example

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ class CreateCircle(Scene):
         circle = Circle()
         circle.set_fill(PINK, opacity=0.5)
         self.play(Create(circle))
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)
+
+        square.next_to(circle, RIGHT, buff=0.5)
+        self.play(Create(circle), Create(square))

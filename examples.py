502968d8d8fb949796bb782c1da4edff023f49ec
jarnesino <julianezequielarnesino@gmail.com>
2025-02-11 11:20:49 -0300

    Add another working example

--- examples.py
+++ examples.py
@@ -18,3 +18,16 @@ class SquareAndCircle(Scene):
 
         square.next_to(circle, RIGHT, buff=0.5)
         self.play(Create(circle), Create(square))
+
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+
+        self.play(Create(square))
+        self.play(square.animate.rotate(PI / 4))
+        self.play(Transform(square, circle))
+        self.play(
+            square.animate.set_fill(PINK, opacity=0.5)
+        )
